5325f6abb0913751fb389f3f3dda21824a5c0bc6
maxjeffwell <maxjeffwell@gmail.com>
2018-07-30 16:42:09 -0400

    updated server for search function

--- server.js
+++ server.js
@@ -10,13 +10,18 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-  res.json(data);
+  if (req.query.searchTerm) {
+    return res.json(data.filter(element =>
+      element.title.includes(req.query.searchTerm)));
+  } 
+  else {
+    return res.json(data);
+  }
 });
 
 app.get('/api/notes/:id', (req, res) => {
   res.json(data.find(element => element.id === parseInt(req.params.id)));
 });
-
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
