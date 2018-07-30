06f0bdc9e1d94ec643d14f247969de360340e08e
maxjeffwell <maxjeffwell@gmail.com>
2018-07-30 15:08:58 -0400

    created GET notes list endpoint

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
 });
+
+
 
 
