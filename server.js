af263e5719408a3e4fdebf5c1605f64ff635c181
maxjeffwell <maxjeffwell@gmail.com>
2018-07-30 14:58:15 -0400

    created express app and started static server

--- server.js
+++ server.js
@@ -3,6 +3,16 @@
 // Load array of notes
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+const express = require('express');
 
-// INSERT EXPRESS APP CODE HERE...
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+
+
