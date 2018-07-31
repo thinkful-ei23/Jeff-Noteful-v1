df0cc24e434fd145ea72e7c9bb261a5ee58167b5
maxjeffwell <maxjeffwell@gmail.com>
2018-07-31 17:04:33 -0400

    added error handler

--- server.js
+++ server.js
@@ -4,12 +4,15 @@
 const data = require('./db/notes');
 
 const express = require('express');
+const { PORT } = require('./config');
+const { myLogger } = require('./middleware/logger');
 // const morgan = require('morgan');
 
 const app = express();
 // app.use(morgan('dev'));
 
 app.use(express.static('public'));
+app.use(myLogger);
 
 app.get('/api/notes', (req, res) => {
   if (req.query.searchTerm) {
@@ -25,7 +28,21 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(data.find(element => element.id === parseInt(req.params.id)));
 });
 
-app.listen(8080, function () {
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
